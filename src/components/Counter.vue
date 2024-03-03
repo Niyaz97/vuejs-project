5ad6ddfa0da5e7175ac5afef99ebfcd3f48318a8
Niyaz Safiullin <niyaz_safiullin@epam.com>
2024-03-03 18:11:41 +0100

    initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+
+  const count = reactive({count: 0});
+  const incr = ()=> {count.count++};
+
+</script>
+
+<template>
+  <div>
+    <h1>{{count.count}}</h1>
+  </div>
+  <button @click="incr">increment</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/CampingView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Counter from '@/components/Counter.vue';
+</script>
+<template>
+  <div class="camping">
+    <h1>This is a camping page</h1>
+    <Counter></Counter>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .camping {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
