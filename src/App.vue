bb3ff84b1fc69636bf9e6295cf77adf90e4c84b9
Niyaz Safiullin <niyaz_safiullin@epam.com>
2024-03-03 18:43:27 +0100

    Add auth and amplify config

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { Authenticator } from "@aws-amplify/ui-vue";
+  import "@aws-amplify/ui-vue/styles.css";
+
+  import { Amplify } from 'aws-amplify';
+  import awsconfig from './aws-exports';
+
+  Amplify.configure(awsconfig);
+
 </script>
 
 <template>
@@ -14,6 +22,13 @@ import { RouterLink, RouterView } from 'vue-router'
   </header>
 
   <RouterView />
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
+
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Amplify from 'aws-amplify';
+import awsExports from './aws-exports'; // your generated aws-exports file
+
+Amplify.configure(awsExports);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +17,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+      find: './runtimeConfig',
+      replacement: './runtimeConfig.browser', // ensures browser compatible version of AWS JS SDK is used
     }
   }
 })
